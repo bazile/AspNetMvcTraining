16141cc9f1f725101002306ea464521d714cfd5f
bazile <vasily.petruhin@gmail.com>
2018-12-09 11:08:55 +0300

    История вычислений и настройки

--- CalcWebApplication/CalcWebApplication/Controllers/HomeController.cs
+++ CalcWebApplication/CalcWebApplication/Controllers/HomeController.cs
@@ -12,6 +12,10 @@ namespace CalcWebApplication.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            ViewData["a"] = Request.Cookies["a"]?.Value ?? "";
+            ViewData["op"] = Request.Cookies["op"]?.Value ?? "";
+            ViewData["b"] = Request.Cookies["b"]?.Value ?? "";
+
             return View();
         }
 
@@ -29,10 +33,58 @@ namespace CalcWebApplication.Controllers
                 case "/": result = model.A / model.B; break;
             }
 
+            if (result.HasValue)
+            {
+                var prevCalculations = (List<CalcModel>)Session["prevCalculations"];
+                if (prevCalculations == null)
+                {
+                    prevCalculations = new List<CalcModel>();
+                    Session["prevCalculations"] = prevCalculations;
+                }
+                prevCalculations.Add(model);
+            }
+
             return View(new CalcViewModel
             {
                 A = model.A, B = model.B, Op = model.Op, Result = result
             });
+        }
+
+        public ActionResult Configure()
+        {
+            ViewData["a"] = Request.Cookies["a"]?.Value ?? "";
+            ViewData["op"] = Request.Cookies["op"]?.Value ?? "";
+            ViewData["b"] = Request.Cookies["b"]?.Value ?? "";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Configure(string a, string op, string b)
+        {
+            DateTime defaultExpire = DateTime.Now.AddYears(1);
+
+            if (int.TryParse(a, out _))
+            {
+                // "Постоянная" cookie
+                Response.Cookies.Add(new HttpCookie("a", a.Trim()) { Expires = defaultExpire });
+            }
+
+            if (int.TryParse(b, out _))
+            {
+                // "Постоянная" cookie
+                Response.Cookies.Add(new HttpCookie("b", b.Trim()) { Expires = defaultExpire });
+            }
+
+            string[] validOperators = { "+", "-", "/", "*" };
+            if (validOperators.Contains(op))
+            {
+                // Сессионная cookie
+                //Response.Cookies.Add(new HttpCookie("op", op));
+                // "Постоянная" cookie
+                Response.Cookies.Add(new HttpCookie("op", op) { Expires = defaultExpire });
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
