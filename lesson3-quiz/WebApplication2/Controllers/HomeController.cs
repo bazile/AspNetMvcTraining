dcae0dfe9ec71adfe0c40225d5e5408ea52533da
Vasily <vasily.petruhin@gmail.com>
2019-02-19 22:09:13 +0300

    quizz

---
+++ lesson3-quiz/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : BaseController
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var quizzes = LoadQuizzes();
+            return View(quizzes);
+        }
+    }
+}
---
+++ lesson3-quiz/WebApplication2/Models/Quizz.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+
+//namespace WebApplication2.Models
+//{
+//    public class Quizz
+//    {
+//        public int Index { get; set; }
+//        public string Name { get; set; }
+//        public Question[] Questions { get; set; }
+//    }
+//}
