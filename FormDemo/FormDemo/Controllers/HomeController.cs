62ed337a50ec65d6d12a9805b520d75f9c5266c1
Vasily <vasily.petruhin@gmail.com>
2019-02-19 22:08:44 +0300

    Работа с формами

---
+++ FormDemo/FormDemo/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SayHello()
+        {
+            return View();
+        }
+
+        public ActionResult SayHelloHandler(string personName)
+        {
+            string someInput = Request.QueryString["someInput"];
+
+            return View("SayHello", (object)personName);
+        }
+
+        public ActionResult Calc()
+        {
+            return View();
+        }
+
+        public ActionResult CalcHandler(int num1, string op, int num2)
+        {
+            double result = 0;
+            switch (op)
+            {
+                case "+": result = num1 + num2; break;
+                case "-": result = num1 - num2; break;
+                case "*": result = num1 * num2; break;
+                case "/": result = (double)num1 / num2; break;
+            }
+            return View("Calc", result);
+        }
+    }
+}
