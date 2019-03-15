74ed34da80ee95ea475859b2b36c63194db64f5c
Vasily <vasily.petruhin@gmail.com>
2019-03-15 16:37:56 +0300

    Some snippets

---
+++ _snippets/hash.sha1.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography;
+
+byte[] GetSHA1(byte[] data)
+{
+    using (var sha1 = SHA1.Create())
+    {
+        return sha1.ComputeHash(data);
+    }
+}
---
+++ _snippets/hex.cs
@@ -0,0 +1,29 @@
+﻿static string ToHexString(byte[] array)
+{
+    if (array == null || array.Length == 0) return "";
+    var sb = new StringBuilder(array.Length*2);
+    for (int i = 0; i < array.Length; i++)
+    {
+        sb.Append(array[i].ToString("X2"));
+    }
+    return sb.ToString();
+}
+
+static byte[] FromHexString(string hex)
+{
+    if (hex == null || hex.Length == 0) return Array.Empty<byte>();
+    if (hex.Length%2 == 1) throw new InvalidOperationException("String length must be even number");
+    
+    byte[] array = new byte[hex.Length/2];
+    for (int i = 0; i < hex.Length; i+=2)
+    {
+        array[i/2] = (byte)((GetHexVal(hex[i]) << 4) + (GetHexVal(hex[i + 1])));
+    }
+    return array;
+}
+
+public static int GetHexVal(char hex)
+{
+    int val = (int)hex;
+    return val - (val < 58 ? 48 : (val < 97 ? 55 : 87));
+}
