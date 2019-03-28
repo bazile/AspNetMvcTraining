35f3323a03cc4d0635dae301c82ba4b8ae1287d7
bazile <vasily.petruhin@gmail.com>
2019-03-28 21:59:13 +0300

    async/await

--- LifeCycleDemo/LifeCycleDemo/Controllers/HomeController.cs
+++ LifeCycleDemo/LifeCycleDemo/Controllers/HomeController.cs
@@ -2,7 +2,9 @@
 using LifeCycleDemo.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,44 +12,44 @@ namespace LifeCycleDemo.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index(int? page)
+        public async Task<ActionResult> Index(int? page)
         {
             const int PAGE_SIZE = 2;
             page = page ?? 1;
 
             using (var db = new WallpaperContext())
             {
-                int totalCount = db.Wallpapers.Count(); // SELECT COUNT(*) FROM Wallpaper
+                int totalCount = await db.Wallpapers.CountAsync(); // SELECT COUNT(*) FROM Wallpaper
 
                 var model = new WallpaperListViewModel { Page = page.Value };
-                model.Wallpapers = db.Wallpapers
+                model.Wallpapers = await db.Wallpapers
                     .Select(w => new WallpaperViewModel
                     {
                         Id = w.Id,
                         Name = w.Name
                     }) // SELECT Id, Name FROM Wallparer
                     .OrderBy(w => w.Id)
-                    .Skip((page.Value - 1)*PAGE_SIZE).Take(PAGE_SIZE).ToArray();
+                    .Skip((page.Value - 1)*PAGE_SIZE).Take(PAGE_SIZE).ToArrayAsync();
                 return View(model);
             }
         }
 
-        public ActionResult Wallpaper(int id)
+        public async Task<ActionResult> Wallpaper(int id)
         {
             using (var db = new WallpaperContext())
             {
-                var wallpaper = db.Wallpapers.Find(id);
+                var wallpaper = await db.Wallpapers.FindAsync(id);
                 if (wallpaper == null) return HttpNotFound();
 
                 return View(new WallpaperViewModel { Id = wallpaper.Id } );
             }
         }
 
-        public ActionResult WallpaperImage(int id)
+        public async Task<ActionResult> WallpaperImage(int id)
         {
             using (var db = new WallpaperContext())
             {
-                var wallpaper = db.Wallpapers.Find(id);
+                var wallpaper = await db.Wallpapers.FindAsync(id);
                 if (wallpaper == null) return HttpNotFound();
 
                 return new FileContentResult(wallpaper.ImagesBytes, "image/jpeg");
---
+++ LifeCycleDemo/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Thread fillThread = new Thread(FillListBox);
+            //fillThread.Start();
+            ThreadPool.QueueUserWorkItem(FillListBox);
+        }
+
+        void FillListBox(object dummy)
+        {
+            //string s = "aa,bb,cc";
+            //string[] arr = s.Split(',');
+            //Span<T>
+            //Memory<T>
+
+            for (int i = 0; i < 5; i++)
+            {
+                //textBox1.AppendText($"Строка #{i + 1}\r\n");
+                listBox1.Items.Add($"Строка #{i + 1}");
+                Thread.Sleep(750);
+            }
+        }
+    }
+}
